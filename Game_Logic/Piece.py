341fce879917992d7f02539efe4c2f3e44671677
a7med31 <ahmed.abdulrahman3000@gmail.com>
2024-11-29 20:57:14 +0200

    get moves for ant piece

--- Game_Logic/Piece.py
+++ Game_Logic/Piece.py
@@ -49,3 +49,43 @@ class Grasshopper(Piece):
                     moves.append((q, r))
                 jumped = False
         return moves
+    
+
+class Ant(Piece):
+
+    def __init__(self, owner, position=None):
+        """
+        Initializes an Ant Piece
+
+        """
+        super().__init__(owner, position)
+
+    def getMoves(self, board) -> list:
+        """
+        Returns a list of all valid moves for the Ant.
+        
+        """
+        if self.position is None:
+            return []  
+
+        visited = set()
+        valid_moves = set()
+
+        def dfs(position):
+            """Depth-first search to find all reachable positions."""
+            for neighbor in board.get_neighbors(*position):
+                if neighbor not in visited and not board.hasPieceAt(*neighbor):
+                    neighbors = board.get_neighbors(*neighbor)
+                    visited.add(neighbor)
+                    valid_moves.add(neighbor)
+                    if all( not board.hasPieceAt(*neighbor) for neighbor in neighbors):
+                        dfs(neighbor)
+                   
+
+        # Start DFS from the current position
+        visited.add(self.position)
+        dfs(self.position)
+
+        return list(valid_moves)
+    
+    
