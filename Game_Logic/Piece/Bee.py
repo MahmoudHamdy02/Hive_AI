5ae7f47977907282442463f7fb2d020c48279e4d
Shadi mohamed <shadymohamed727@gmail.com>
2024-11-29 22:44:16 +0200

    add Bee piece
    add Bee piece

---
+++ Game_Logic/Piece/Bee.py
@@ -0,0 +1,26 @@
+class Bee(Piece):
+    def __init__(self, owner, position=None):
+        """
+        Initializes a Bee Piece.
+        The Bee moves one space in any direction.
+        """
+        super().__init__(owner, position)
+
+    def getMoves(self, board) -> list:
+        """
+        
+        Returns a list of all possible moves for the Bee.
+        
+        """
+        
+        directions = [(1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)]
+        moves = []
+        
+        if self.position:
+            q, r = self.position
+            for dq, dr in directions:
+                nq, nr = q + dq, r + dr
+                if not board.hasPieceAt(nq, nr):  # The Bee can't move to a square with a piece
+                    moves.append((nq, nr))
+
+        return moves
