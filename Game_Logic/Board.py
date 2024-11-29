e64f0d2ce25e8baca6786dd43ac13842afce377b
Mohammed Yasser <mohdyasser100@gmail.com>
2024-11-29 14:57:09 +0200

    add: grasshopper logic

---
+++ Game_Logic/Board.py
@@ -0,0 +1,17 @@
+class Board:
+    def __init__(self) -> None:
+        self.grid = {} # (q, r): [Piece1, Piece2, ...]}
+
+    def addPiece(self, piece, q, r) -> None:
+        if (q,r) not in self.grid:
+            self.grid[(q,r)] = []
+        self.grid[(q,r)].append(piece)
+
+    def movePiece(self, piece, q, r) -> None:
+        self.grid[piece.position].remove(piece)
+        self.addPiece(piece, q, r)
+        piece.move(q, r)
+        
+    def hasPieceAt(self, q,r) -> bool:
+        return ((q,r) in self.grid and len(self.grid[(q,r)]) > 0)
+    
--- Game_Logic/Piece.py
+++ Game_Logic/Piece.py
@@ -1,4 +1,4 @@
-import Player
+import Player, Board
 
 class Piece:
     def __init__(self, owner, position=None):
@@ -9,9 +9,43 @@ class Piece:
         self.owner: Player = owner
         self.position: tuple = position
 
-    def getMoves(self) -> list:
+    def getMoves(self, board: Board) -> list:
         """
         Returns a list of all possible moves for the piece
 
         """
         raise NotImplementedError("getMoves() must be implemented by subclass")
+    
+    def move(self, q, r):
+        """
+        Moves the piece to the specified position
+
+        """
+        self.position = (q, r)
+
+class Grasshopper(Piece):
+    def __init__(self, owner, position=None):
+        """
+        Initializes a Grasshopper Piece
+
+        """
+        super().__init__(owner, position)
+
+    def getMoves(self, board) -> list:
+        """
+        Returns a list of all possible moves for the Grasshopper
+
+        """
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, 1)]
+        moves = []
+        jumped = False
+        if self.position:
+            for dq, dr in directions:
+                q, r = self.position[0]+dq, self.position[1]+dr
+                while board.hasPieceAt(q, r):
+                    q, r = q+dq, r+dr
+                    jumped = True
+                if jumped:
+                    moves.append((q, r))
+                jumped = False
+        return moves
