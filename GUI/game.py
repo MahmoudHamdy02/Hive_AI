c3f2728285144d84f9429b74ca5f2b512b8baf86
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2024-11-30 02:36:09 +0200

    force turn order

--- GUI/game.py
+++ GUI/game.py
@@ -2,6 +2,7 @@ import pygame
 from hex_manager import HexManager
 from constants import *
 from Player_widget import PlayerWidget
+
 player_dict = {
     "ant": 2,
     "bee": 2,
@@ -9,6 +10,7 @@ player_dict = {
     "hopper": 7, 
     "beetle": 1
 }
+
 player1 = PlayerWidget("Player 1",(255, 0, 0) , player_dict)
 player2 = PlayerWidget("salma waleed",(0, 0, 255) , player_dict)
 hex_manager = HexManager(ORIGIN, RADIUS, MINIMAL_RADIUS)
@@ -27,6 +29,7 @@ hex_manager.createHexagonTile(2,2)
 vAntMoves=[(0,0),(-1,0),(2,0)]
 for i in vAntMoves:
     hex_manager.drawOutline(i[0],i[1])
+
 def start_game():
     
     # pygame setup
@@ -39,7 +42,7 @@ def start_game():
     bg_image = pygame.transform.scale(bg_image, (640, 1280))
 
     selected_insect = None  # To track the selected insect
-    selected_player = None  # To track the selected player
+    current_player = player1
 
     while running:
         # poll for events
@@ -50,26 +53,25 @@ def start_game():
             elif event.type==pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
 
-                # Check if a player widget was clicked
-                if pygame.Rect(0, 0, 250, HEIGHT).collidepoint(mouse_pos):  # Player 1 area
-                    selected_player = player1
-                elif pygame.Rect(WIDTH - 250, 0, 250, HEIGHT).collidepoint(mouse_pos):  # Player 2 area
-                    selected_player = player2
+                # This currently force the turn order
+                # TODO: A turn must be skipped if there are no available moves
 
-                # Check if clicking an insect box
-                if selected_player and not selected_insect:
-                    selected_insect = selected_player.handle_click(mouse_pos)
+                # Check if player area 1 or 2 was clicked
+                # If yes, get the clicked insect
+                if current_player == player1 and pygame.Rect(0, 0, 250, HEIGHT).collidepoint(mouse_pos) or current_player == player2 and pygame.Rect(WIDTH - 250, 0, 250, HEIGHT).collidepoint(mouse_pos):
+                    selected_insect = current_player.handle_click(mouse_pos)
+
                  # Check if clicking a board position to move the insect
-                elif selected_insect:
+                if selected_insect:
                     for tile in hex_manager.outlines:  # Assuming `vAntMoves` contains hexagonal tiles
                         if tile.contains_point(mouse_pos):
                         # Move the insect to this valid tile
                             q, r=tile.axial_coordinates
                             hex_manager.removeOutline(q, r)
-                            hex_manager.createHexagonTile(q, r, selected_insect, selected_player.color)  # Add insect to tile
-                            selected_player.insects[selected_insect] -= 1  # Decrement insect count
+                            hex_manager.createHexagonTile(q, r, selected_insect, current_player.color)  # Add insect to tile
+                            current_player.insects[selected_insect] -= 1  # Decrement insect count
                             selected_insect = None  # Reset selected insect
-                            selected_player = None  # Reset selected player
+                            current_player = player2 if current_player == player1 else player1  # Reset selected player
                             break
 
 
