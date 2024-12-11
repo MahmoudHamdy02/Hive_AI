ed0cd3e540b1686840f9c97ecdc748c578218980
a7med31 <ahmed.abdulrahman3000@gmail.com>
2024-12-11 15:44:36 +0200

    fix ant connectivity

--- Game_Logic/Piece/Ant.py
+++ Game_Logic/Piece/Ant.py
@@ -13,8 +13,39 @@ class Ant(Piece):
         Returns a list of all valid moves for the Ant.
         
         """
+
         if self.position is None:
             return []  
+        
+        
+        ADJACENT_HEXES = [(1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)]
+        
+        def get_adjacent_hexes(q, r):
+            return [(q + dq, r + dr) for dq, dr in ADJACENT_HEXES]
+    
+        board.grid[self.position].removePiece()
+
+        def is_hive_continuous():
+            visited = set()
+            iterator = iter(board.grid.keys())
+            start = next(iterator)  # Get the first hex in the grid
+            while not board.hasPieceAt(*start):
+                start = next(iterator)
+
+            def dfs(node):
+                visited.add(node)
+                for neighbor in get_adjacent_hexes(*node):
+                    if board.hasPieceAt(*neighbor) and neighbor not in visited:
+                        dfs(neighbor)
+
+            dfs(start)
+            print(len(visited), board.noOfPieces)
+            return len(visited) == board.noOfPieces
+        
+        board.addPiece(self,self.position)
+
+        if not is_hive_continuous:
+            return []
 
         visited = set()
         valid_moves = set()
