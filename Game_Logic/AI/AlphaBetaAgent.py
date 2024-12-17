ec89bcc1973326eaf807e0ed36a94f2d7917849d
Mohammed Yasser <mohdyasser100@gmail.com>
2024-12-17 03:16:26 +0200

    change some things in alpha beta

--- Game_Logic/AI/AlphaBetaAgent.py
+++ Game_Logic/AI/AlphaBetaAgent.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+import random
 from  Game_Logic.AI.Agent import Agent
 from Game_Logic.Game.GameController import GameController
 
@@ -41,16 +41,23 @@ class AlphaBetaAgent(Agent):
         """
         Returns the best value that maximizer can obtain
         """
+        len_moves = 0
+        move_scores = []
         moves = self.originalGameController.get_all_moves_and_adds()
+        # moves = self.order_moves(moves)
+        # moves = moves[:10]
+        if len(moves) > 10:
+            moves = random.sample(moves, 10)
         if depth <= 0 or len(moves) == 0:
             return self.heuristic.calculateBoardScore(self.originalGameController)
     
         if maximizingPlayer:
             maxValue = float('-inf')
             for move in moves:
+                len_moves += 1
                 if not self.doMove(self.originalGameController, move):  
-                    continue  
-                value = self._alphaBeta(self.originalGameController, depth - 1, alpha, beta, False)
+                    continue
+                value = self._alphaBeta(depth - 1, alpha, beta, False)
                 self.undoMove(move)
                 if value == float('inf'):
                     return value
@@ -58,16 +65,20 @@ class AlphaBetaAgent(Agent):
                 maxValue = max(maxValue, value)
                 alpha = max(alpha, value)
                 if alpha >= beta:
-                    break
-
+                    # print(alpha, beta)
+                    # print("pruning")
+                    print("pruned",len_moves)
+                    return maxValue
+            print("not pruned",len_moves)
             return maxValue
         
         else:
             minValue = float('inf')
             for move in moves:
+                len_moves += 1
                 if not self.doMove(self.originalGameController, move):  
                     continue
-                value = self._alphaBeta(self.originalGameController, depth - 1, alpha, beta, True)
+                value = self._alphaBeta(depth - 1, alpha, beta, True)
                 self.undoMove(move)
                 if value == float('-inf'):
                     return value
@@ -75,6 +86,32 @@ class AlphaBetaAgent(Agent):
                 minValue = min(minValue, value)
                 beta = min(beta, value)
                 if alpha >= beta:
-                    break
-
+                    # print(alpha, beta)
+                    # print("pruning")
+                    print("pruned",len_moves)
+                    return minValue
+            
+            # print(alpha, beta)
+            print("not pruned",len_moves)
             return minValue
+        
+    def order_moves(self, moves: list) -> list:
+        """ 
+        Orders the moves based on the heuristic value of the game state after each move
+        """
+        move_values = []
+        for move in moves:
+            self.doMove(self.originalGameController,move)
+            move_value = self.heuristic.calculateBoardScore(self.originalGameController)
+            self.undoMove(move)
+            move_values.append((move_value, move))
+        
+        # Sort moves based on the heuristic values in descending order
+        move_values.sort(reverse=True, key=lambda x: x[0])
+        
+        # Extract the sorted moves
+        sorted_moves = [move for _, move in move_values]
+        values = [value for value, _ in move_values]
+        print(values[:10])
+        
+        return sorted_moves
